49c5c87d17ce6839ebfb5ef29e45a53a9df76cfb
LIZXP <Peter.xli@outlook.com>
2022-08-16 00:58:46 -0600

    create tables and insert datasets in the database

---
+++ db/seeds/03_sales.sql
@@ -0,0 +1,1 @@
+INSERT INTO users (id, user_id,payment_status,order_date) VALUES (1, 1, 'paid', '2022-07-24');
---
+++ db/seeds/04_books_in_order.sql
@@ -0,0 +1,3 @@
+INSERT INTO users (id, order_id,book_id) VALUES (1, 1, 53);
+INSERT INTO users (id, order_id,book_id) VALUES (2, 1, 54);
+INSERT INTO users (id, order_id,book_id) VALUES (3, 1, 55);
