4ce3de9597803a2ed21ea61049f9472124082ee4
LIZXP <Peter.xli@outlook.com>
2022-08-22 20:29:10 -0600

    fix the routes path in server

--- routes/userLogout.js
+++ routes/userLogout.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const app = express();
+const cookieParser = require("cookie-parser");
 app.use(cookieParser());
 
 module.exports = (db) => {
