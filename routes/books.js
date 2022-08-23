3cc9b4a5bb1cc13cdb24a96f40f14be9ff125d4f
LIZXP <Peter.xli@outlook.com>
2022-08-23 11:49:13 -0600

    add the user data from database to render findallbooks page

--- routes/books.js
+++ routes/books.js
@@ -1,18 +1,27 @@
-const express = require('express');
-const router  = express.Router();
-const {getAllBooks} = require("../db/server/database");
+const express = require("express");
+const router = express.Router();
+const { getAllBooks, getUserWithId } = require("../db/server/database");
 
 module.exports = () => {
   router.get("/findBooks", (req, res) => {
-    getAllBooks()
-      .then(books => {
-        console.log(books);
-        res.render("findBooks", {books});
+    userId = req.cookies.userId;
+    getUserWithId(userId)
+      .then((user) => {
+        getAllBooks()
+          .then((books) => {
+            console.log(books, user);
+            const tempVars = {
+              user,
+              books,
+            };
+            res.render("findBooks", tempVars);
+          })
+          .catch((err) => {
+            res.status(500).json({ error: err.message });
+          });
       })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
+      .catch((err) => {
+        console.log(err);
       });
   });
   return router;
