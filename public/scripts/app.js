766e1acd439d402607d26d4680fa90455ac95f2f
Chris Bowyer <100442597+Devcab2@users.noreply.github.com>
2022-08-24 21:14:20 -0600

    created login page + implement logic and fix route to log in using users emails  in db

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,6 +1,36 @@
 // Client facing scripts here
 
+
 // import jQuery file to render conversations view
 
-const conversations = require("./conversations");
-conversations();
+// signIn function
+
+$(document).ready(function() {
+
+
+  const errorMessage = (msg) => {
+    $("#error-message").html(msg);
+    $("#error-message").slideDown();
+  };
+
+  const signIn = function() {
+    $("form").on("submit", function(event) {
+      event.preventDefault();
+      const text = $("form input").val();
+      console.log(text);
+      if (text.length === 0) {
+        errorMessage("\u2A02 Please enter your email in order to sign in. \u2A02");
+      } else {
+        $.post("/api/login", $(this).serialize())
+          .then(() => {
+            $("#error-message").hide();
+            window.location.href = window.location.origin + "/api/books";
+          })
+          .catch(err => {
+            console.log(err.responseText);
+          });
+      }
+    });
+  };
+ signIn();
+});
---
+++ routes/books.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const router = express.Router();
+const {  addNewBooks, getAllBooks, getUserWithId } = require("../db/server/database");
+
+module.exports = () => {
+  router.get("/", (req, res) => {
+    const userId = req.cookies.userId;
+    getUserWithId(userId)
+      .then((user) => {
+        getAllBooks()
+          .then((books) => {
+            const tempVars = {
+              user,
+              books,
+            };
+            return res.render("findBooks", tempVars);
+          })
+          .catch((err) => {
+            console.log(err);
+            res.redirect("/home");
+          });
+      })
+      .catch((err) => {
+        console.log(err);
+        res.redirect("/home");
+      });
+  });
+
+  router.post("/", (req, res) => {
+    const userId = req.cookies.userId;
+    const books = req.body;
+    getUserWithId(userId)
+      .then((user) => {
+        if (user.user_type === admin) {
+          addNewBooks(userId, books)
+            .then((newBook) => {
+              res.json(newBook);
+            })
+            .catch((e) => {
+              console.log(e);
+              res.redirect("/home");
+            });
+        } else {
+          res.status(401).send("Only seller will be able to add books");
+          res.redirect("/login");
+        }
+      })
+      .catch((e) => {
+        console.log(e);
+        res.redirect("/home");
+      });
+  });
+  return router;
+};
