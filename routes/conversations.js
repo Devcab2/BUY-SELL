80f60e22df930246b4f072c77b624e37fffab82d
Chris Bowyer <100442597+Devcab2@users.noreply.github.com>
2022-08-27 10:38:42 -0600

    final fixes

--- routes/conversations.js
+++ routes/conversations.js
@@ -17,46 +17,6 @@ module.exports = () => {
       });
   });
 
-  // router.get("/newConversation/:bookId/:conversation", (req, res) => {
-  //   const userId = req.cookies.userId;
-  //   const bookId = req.params.bookId;
-  //   const conversationId = req.params.conversation;
-  //   console.log("purchasingUserId:", userId);
-  //   console.log("bookId:", bookId);
-  //   console.log("conversationId:", conversationId);
-  //   getBookSellerName(bookId)
-  //     .then((seller) => {
-  //       console.log("seller:", seller);
-  //       getAllUserConversations(userId)
-  //         .then((conversationsArray) => {
-  //           for (const conversation of conversationsArray) {
-  //             console.log("conversation:", conversation);
-  //             getDetailsForSpecificConversation(conversation.conversation_id)
-  //               .then((details) => {
-  //                 console.log("conversations:", conversationsArray);
-  //                 console.log("details:", details);
-  //                 const tempVars = { userId, seller: seller.seller, bookId, conversations: conversationsArray, details };
-  //                 console.log("seller:",seller.seller);
-  //                 res.render("conversations", tempVars);
-  //               })
-  //               .catch((err) => {
-  //                 console.log(err);
-  //                 res.redirect("/api/books");
-  //               });
-  //           }
-
-  //         })
-  //         .catch((err) => {
-  //           console.log(err);
-  //           res.redirect("/api/books");
-  //         });
-  //     })
-  //     .catch((err) => {
-  //       console.log(err);
-  //       res.redirect("/api/books");
-  //     });
-  // });
-
   router.post("/", (req, res) => {
     const userId = req.cookies.userId;
     const message = req.body;
@@ -64,30 +24,6 @@ module.exports = () => {
     const tempVars = {message, userId};
     res.render("conversations", tempVars);
   });
-
-
-  // router.get("/:userId", (req, res) => {
-  //   const userId = req.cookies.userId;
-  //   console.log("userID:", userId);
-  //   getAllUserConversations(userId)
-  //     .then((conversations) => {
-  //       console.log("conversations:", conversations);
-
-  //       getMessagesForSpecificConversation(conversations)
-  //         .then((messages) => {
-  //           const tempVars = {messages, userId};
-  //           console.log("messages:", messages);
-  //           res.redirect("conversations", tempVars);
-  //         })
-  //         .catch((err) => {
-  //           console.log(err);
-  //           res.redirect("findBooks");
-  //         });
-  //     });
-  // });
-  // router.get("/", (req, res) => {
-  //   res.render("conversations");
-  // });
 
   return router;
 };
